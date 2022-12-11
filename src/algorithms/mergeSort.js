73e0a51549fbe1c0e986577c69d3f59c55b0dcae
erikmalm <70799618+erikmalm@users.noreply.github.com>
2022-12-11 14:40:13 +0100

    updated all files

--- src/algorithms/mergeSort.js
+++ src/algorithms/mergeSort.js
@@ -27,6 +27,8 @@ function mergeSort(array) {
   }
   result.push(...sortedLeft, ...sortedRight);
 
+  console.log(result);
+
   return result;
 }
 
--- src/presenters/mergeSortVisualizationPresenter.js
+++ src/presenters/mergeSortVisualizationPresenter.js
@@ -2,43 +2,32 @@ import React, { useState } from "react";
 import MergeSortVisualizationView from "../views/mergeSortVisualizationView";
 import mergeSort from "../algorithms/mergeSort";
 
-// The MergeSortVisualizationPresenter component is a class
-// component that manages the state and logic for the
-// merge sort visualization. It receives the number of
-// elements to sort as a prop, and it generates a random
-// array of that length. It also provides a method for
-// sorting the array using the mergeSort algorithm, and
-// for updating the state of the MergeSortVisualizationView
-// component.
-class MergeSortVisualizationPresenter extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // Initialize the state with a random array of the
-    // specified length.
-    this.state = {
-      array: this.generateRandomArray(props.numElements),
-    };
-  }
+// The MergeSortVisualizationPresenter function is a
+// function component that manages the state and logic
+// for the merge sort visualization. It receives the
+// number of elements to sort as a prop, and it
+// generates a random array of that length. It also
+// provides a method for sorting the array using the
+// mergeSort algorithm, and for updating the state of
+// the MergeSortVisualizationView component.
+const MergeSortVisualizationPresenter = (props) => {
+  // Use the useState hook to initialize the state with
+  // a random array of the specified length.
+  const [array, setArray] = useState(generateRandomArray(props.numElements));
 
   // Generate a random array of the specified length.
-  generateRandomArray(length) {
+  function generateRandomArray(length) {
     return Array.from({ length }, () => Math.floor(Math.random() * 100));
   }
 
   // Sort the array using the mergeSort algorithm.
-  sort() {
-    this.setState({ array: mergeSort(this.state.array) });
+  function sort() {
+    setArray(mergeSort(array));
   }
 
-  render() {
-    return (
-      <MergeSortVisualizationView
-        array={this.state.array}
-        onSort={() => this.sort()}
-      />
-    );
-  }
-}
+  // Return the MergeSortVisualizationView component with
+  // the array and onSort props.
+  return <MergeSortVisualizationView array={array} onSort={sort} />;
+};
 
 export default MergeSortVisualizationPresenter;
